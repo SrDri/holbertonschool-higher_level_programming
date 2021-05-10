62439382e9bebf7adead7c782eabc8796cf279fc
SrDri <juanjcarabali@gmail.com>
2021-05-10 09:40:38 +0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,9 @@
 
 def roman_to_int(roman_string):
     if roman_string and isinstance(roman_string, str) == True:
-        roman = {"I": 1, "V": 5, "X": 10, "L": 50,
-                 "C": 100, "D": 500, "M": 1000}
+        roman = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+        }
         prev = 0
         resultado = 0
 
