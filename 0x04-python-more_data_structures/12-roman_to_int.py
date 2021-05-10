a8bce15a8d8ff0394bf593be75fed8fa9694f119
SrDri <juanjcarabali@gmail.com>
2021-05-10 09:43:21 +0700

    Fix pep8 task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string and isinstance(roman_string, str) == True:
+    if roman_string and isinstance(roman_string, str) is True:
         roman = {
             "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
         }
