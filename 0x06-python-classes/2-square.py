94337097a509285dd19ebcb609276ffaa914be70
SrDri <juanjcarabali@gmail.com>
2021-05-19 06:48:13 +0700

    Fix syntax

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Constructor"""
 
-        if isinstance(size, int) != True:
+        if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
             return
 
