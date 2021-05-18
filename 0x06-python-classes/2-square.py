0a1817e83500abbff1889f42036fb621962af21a
SrDri <juanjcarabali@gmail.com>
2021-05-19 06:43:58 +0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Class"""
+
+    def __init__(self, size=0):
+        """Constructor"""
+        if isinstance(size, int) != True:
+            raise TypeError("size must be an integer")
+            return
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+            return
+
+        self.__size = size
