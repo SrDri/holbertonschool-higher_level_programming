10243aa219104de9a1da55ee8e295149619d79de
SrDri <juanjcarabali@gmail.com>
2021-05-19 07:52:31 +0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """class"""
+
+    def __init__(self, size=0):
+        """Constructor"""
+        self.size = size
+
+    @property
+    def size(self):
+        """get __size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set __size"""
+        if value:
+            if isinstance(value, int) is not True:
+                raise TypeError("size must be an integer")
+                return
+
+            if value < 0:
+                raise ValueError("size must be >= 0")
+                return
+
+        self.__size = value
+
+    def area(self):
+        """Return size"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print stdout square"""
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(0, self.size):
+                print("#" * self.size)
