ecf3fc84f46835727ab6db22769a710351736283
SrDri <juanjcarabali@gmail.com>
2021-05-27 04:53:33 +0700

    Fix task 0 mode 3 ran tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,18 +7,24 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_max(self):
-        self.assertEqual(max_integer([1, 3, 23]), 23)
-        self.assertEqual(max_integer([-1, -3, -23]), -1)
+    def test_basic(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5, 6]), 6)
+
+    def max_beging(self):
+        self.assertEqual(max_integer([10, 2, 3, 4, 5, 6]), 10)
+
+    def max_middle(self):
+        self.assertEqual(max_integer([1, 2, 20, 4, 5]), 20)
         self.assertEqual(max_integer([-1, -0.5, -23]), -0.5)
-        self.assertEqual(max_integer([-1]), -1)
 
-    def test_list(self):
-        self.assertIsNone(max_integer([]))
-        self.assertIsNone(max_integer())
+    def test_basic_negative(self):
+        self.assertEqual(max_integer([-5, -1]), -1)
 
-    def test_type(self):
-        self.assertIsInstance([], list)
+    def one_element(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_list_empty(self):
+        self.assertEqual(max_integer([]), None)
 
 
 if __name__ == '__main__':
