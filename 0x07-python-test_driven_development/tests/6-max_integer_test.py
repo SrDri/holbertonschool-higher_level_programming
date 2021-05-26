5537b9feb23eab4b1752f59ccda6aa6fee680da1
SrDri <juanjcarabali@gmail.com>
2021-05-27 04:35:19 +0700

    Task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,14 +6,6 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def max_beging(self):
-        self.assertEqual(max_integer([10, 2, 3, 4, 5, 6]), 10)
-
-    def max_middle(self):
-        self.assertEqual(max_integer([1, 2, 3, 20, 4, 5, 6]), 20)
-
-    def one_element(self):
-        self.assertEqual(max_integer([1]), 1)
 
     def test_basic(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5, 6]), 6)
@@ -35,6 +27,15 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_negative_positive(self):
         self.assertEqual(max_integer([-4, -2, 0, 2, 4]), 4)
+
+    def max_beging(self):
+        self.assertEqual(max_integer([10, 2, 3, 4, 5, 6]), 10)
+
+    def max_middle(self):
+        self.assertEqual(max_integer([1, 2, 20, 4, 5]), 20)
+
+    def one_element(self):
+        self.assertEqual(max_integer([1]), 1)
 
 
 if __name__ == '__main__':
