ec77040f776b0ecb64fb50365c5c7f090df903ba
SrDri <juanjcarabali@gmail.com>
2021-06-02 05:07:23 +0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" is_kind_of_class function """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Instance class """
+
+    resultado = isinstance(obj, a_class)
+    return(resultado)
