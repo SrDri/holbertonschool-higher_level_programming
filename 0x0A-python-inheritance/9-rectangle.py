1309c655c7f0992df53618fdce00f3c5730d1b86
SrDri <juanjcarabali@gmail.com>
2021-06-02 08:02:26 +0700

    Task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" `Rectangle` class """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ The Rectangle class """
+
+    def __init__(self, width, height):
+        """ Constructor class """
+        self.integer_validator('width', width)
+        self.__width = width
+
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def __str__(self):
+        """ Print method """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def area(self):
+        """ Area the square """
+        return self.__height * self.__width
