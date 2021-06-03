5fa21eb44fa8881e1dbd29cd827fbbeb1d9af634
SrDri <juanjcarabali@gmail.com>
2021-06-04 03:49:28 +0700

    Update task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -25,3 +25,7 @@ class Student:
             return aux
 
         def reload_from_json(self, json):
+            for key, value in json.items():
+                if key is self.__dict__:
+                    self.__dict__[key] = value
+                    
