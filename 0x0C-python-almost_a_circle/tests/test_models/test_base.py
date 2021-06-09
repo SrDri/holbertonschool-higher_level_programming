2d459ca0bb1f6536f2cb904e7d70242cbebcff91
SrDri <juanjcarabali@gmail.com>
2021-06-09 08:49:27 +0700

    Test prueba test task 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,3 +6,21 @@ import os
 
 from models.base import Base
 from models.rectangle import Rectangle
+
+
+class TestBaseClass(unittest.TestCase):
+    """ Class test base"""
+
+    def setUp(self):
+        """ runs unit test"""
+        Base._Base__nb_objects = 0
+
+    def test_docstring(self):
+        """ test docstring """
+        self.assertIsNotNone(Base.__doc__)
+
+    def test_pep8(self):
+        """ pep8 """
+        style = pep8.StyleGuide(quiet=True)
+        pep = style.check_files(["models/base.py"])
+        self.assertEqual(pep.total_errors, 0)
