892a594331a63ff1a1a4be65d7d9baa226333496
SrDri <juanjcarabali@gmail.com>
2021-06-09 09:41:01 +0700

    Update test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -25,6 +25,31 @@ class TestBaseClass(unittest.TestCase):
         pep = style.check_files(["models/base.py"])
         self.assertEqual(pep.total_errors, 0)
 
+    def test_init_id(self):
+        """ init id 0+ """
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base().id, 2)
+
+    def test_init_non_empty_id(self):
+        """ init with argument """
+        self.assertTrue(Base(1), 1)
+
+    def test_init_2(self):
+        """ init 2 instances """
+        b1 = Base(69)
+        b2 = Base()
+        self.assertTrue(b1.id == 69)
+        self.assertTrue(b2.id == 1)
+
+    def test_init_of_tree_instances(self):
+        """ init 3 instances """
+        b1 = Base()
+        b2 = Base(69)
+        b3 = Base()
+        self.assertTrue(b1.id == 1)
+        self.assertTrue(b2.id == 69)
+        self.assertTrue(b3.id == 2)
+
 
 if __name__ == '__main__':
     unittest.main()
