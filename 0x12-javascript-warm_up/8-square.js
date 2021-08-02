eb230bdb9a81d39337cbe25e3072452628a52d78
SrDri <juanjcarabali@gmail.com>
2021-08-02 23:52:48 +0700

    Fix syntax

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,8 +5,9 @@ const { argv } = require('process');
 if (isNaN(argv[2]) || !argv[2]) {
   console.log('Missing size');
 } else {
+  let fila = '';
   for (let i = 0; i < argv[2]; i++) {
-    let fila = '';
+    fila = '';
     for (let j = 0; j < argv[2]; j++) {
       fila = fila + 'X';
     }
