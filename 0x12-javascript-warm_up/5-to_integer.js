9554eb9febaa50d5013ce2b89757ae9fa439dc31
SrDri <juanjcarabali@gmail.com>
2021-08-02 23:13:33 +0700

    Fix semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('process');
 
-if (isNaN(argv[2]) == false) {
-	console.log('My number: ' + argv[2])
+if (isNaN(argv[2]) === false) {
+  console.log('My number: ' + argv[2]);
 } else {
-	console.log('Not a number')
+  console.log('Not a number');
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 const frases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < frases.length; i++) {
-	console.log(frases[i])
+  console.log(frases[i]);
 }
