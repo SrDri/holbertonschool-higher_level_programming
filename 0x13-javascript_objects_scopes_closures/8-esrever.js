1dab10e73cd7ae2ca62aa416aecc465bae0ba880
SrDri <juanjcarabali@gmail.com>
2021-08-04 05:04:14 +0700

    Fix and update task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
-	const array = [];
+  const array = [];
 
-	for (let i = list.length - 1; i >= 0; --i) {
-		array.push(list[i]);
-	}
-	return array;
+  for (let i = list.length - 1; i >= 0; --i) {
+    array.push(list[i]);
+  }
+  return array;
 };
