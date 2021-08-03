89f4638f51517da5dab46bff3a09cedc24247d4e
SrDri <juanjcarabali@gmail.com>
2021-08-04 00:07:14 +0700

    Fix semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		} else {
-			return
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		let fila = '';
-		for (let i = 0; i < this.height; i++) {
-			fila = 'X';
-			for (let j = 1; j < this.width; j++) {
-				fila = fila + 'X';
-			}
-			console.log(fila + '');
-		}
-	}
+  print () {
+    let fila = '';
+    for (let i = 0; i < this.height; i++) {
+      fila = 'X';
+      for (let j = 1; j < this.width; j++) {
+        fila = fila + 'X';
+      }
+      console.log(fila + '');
+    }
+  }
 };
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,30 @@
 module.exports = class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		} else {
-			return
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		let fila = '';
-		for (let i = 0; i < this.height; i++) {
-			fila = 'X';
-			for (let j = 1; j < this.width; j++) {
-				fila = fila + 'X';
-			}
-			console.log(fila + '');
-		}
-	}
+  print () {
+    let fila = '';
+    for (let i = 0; i < this.height; i++) {
+      fila = 'X';
+      for (let j = 1; j < this.width; j++) {
+        fila = fila + 'X';
+      }
+      console.log(fila + '');
+    }
+  }
 
-	rotate() {
-		let aux = this.width;
-		this.width = this.height;
-		this.height = aux;
-	}
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
 
-	double() {
-		this.width = this.width * 2;
-		this.height = this.height * 2;
-	}
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 };
