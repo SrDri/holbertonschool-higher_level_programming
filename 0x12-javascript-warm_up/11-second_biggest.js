48fec429c3f6ff73414fc7cef3ab72a3da8265a1
SrDri <juanjcarabali@gmail.com>
2021-08-03 08:54:46 +0700

    Fix semistandar

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const n_array = process.argv.slice(2);
-const lenght_array = n_array.length;
-if ((lenght_array) < 2) {
-	console.log('0');
+const nArray = process.argv.slice(2);
+const lenghtArray = nArray.length;
+if ((lenghtArray) < 2) {
+  console.log('0');
 } else {
-	n_array.sort((x, y) => x - y);
-	console.log(n_array[lenght_array - 2]);
+  nArray.sort((x, y) => x - y);
+  console.log(nArray[lenghtArray - 2]);
 }
