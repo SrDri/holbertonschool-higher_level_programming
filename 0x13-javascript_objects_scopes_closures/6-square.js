62f31cbb45eb925ab366a521de7ccd07b95b69ab
SrDri <juanjcarabali@gmail.com>
2021-08-04 00:17:28 +0700

    Fix square inherits

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,7 @@
 #!/usr/bin/node
+const SquareDef = require('./5-square.js');
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let fila = '';
-    for (let i = 0; i < this.height; i++) {
-      fila = 'X';
-      for (let j = 1; j < this.width; j++) {
-        fila = fila + 'X';
-      }
-      console.log(fila + '');
-    }
-  }
-
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends SquareDef {
   constructor (size) {
     super(size, size);
   }
