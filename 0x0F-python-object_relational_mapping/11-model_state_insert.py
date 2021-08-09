6bff2359bdbed8c347c188add8a9c2ed4f6a42a9
SrDri <juanjcarabali@gmail.com>
2021-08-09 07:44:32 +0700

    Update task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Adds the State object “Louisiana” to the database """
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) < 4:
+        print("Error: this script requires 3 arguments")
+        exit()
+
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
