7c4c8eeff7e475ded6ab882b64a6d806f76b64bb
SrDri <juanjcarabali@gmail.com>
2021-08-09 07:50:48 +0700

    Update task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Change the name of State from database """
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) < 4:
+        print("Error: this script requires 3 arguments")
+        exit()
+
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    name = {'name': 'New Mexico'}
+    states = session.query(State) \
+        .filter(State.id == 2) \
+        .update(name, synchronize_session=False)
+
+    session.commit()
+    session.close()
