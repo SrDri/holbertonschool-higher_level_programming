7e0fb445d9dba21bdcee6e2972736a8d491119fe
SrDri <juanjcarabali@gmail.com>
2021-09-15 03:57:12 +0700

    Update task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,21 +4,21 @@
 const request = require('request');
 
 request.get(process.argv[2], (error, req, body) => {
-	if (error) {
-		throw error;
-	}
-	const resultado = JSON.parse(body).results;
+  if (error) {
+    throw error;
+  }
+  const resultado = JSON.parse(body).results;
 
-	let counter = 0;
+  let counter = 0;
 
-	resultado.forEach(element => {
-		element.characters.forEach(char => {
-			let id = char.slice(-3, -1).replace('/', '');
-			id = parseInt(id);
-			if (id === 18) {
-				counter++;
-			}
-		});
-	});
-	console.log(counter);
+  resultado.forEach(element => {
+    element.characters.forEach(char => {
+      let id = char.slice(-3, -1).replace('/', '');
+      id = parseInt(id);
+      if (id === 18) {
+        counter++;
+      }
+    });
+  });
+  console.log(counter);
 });
