27115161319e22dc0d249eb0d5bcb492ee8542c5
SrDri <juanjcarabali@gmail.com>
2021-09-15 00:13:36 +0700

    Fix semistandar task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // write file
 
-let fs = require('fs');
+const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
+  if (err) {
+    console.log(err);
+    return;
+  }
 });
