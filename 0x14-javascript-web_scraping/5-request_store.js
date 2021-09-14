04cf0af8a0515765864ef1d46a71846503a37352
SrDri <juanjcarabali@gmail.com>
2021-09-15 04:01:53 +0700

    Fis style

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,12 +5,12 @@ const request = require('request');
 const fs = require('fs');
 
 request.get(process.argv[2], (error, req, body) => {
-	if (error) {
-		throw error;
-	}
-	fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
+  if (error) {
+    throw error;
+  }
+  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
