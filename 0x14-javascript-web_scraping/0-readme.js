53a4a5ed988c98e09c9118e0bdb8789f8eacb2e6
SrDri <juanjcarabali@gmail.com>
2021-09-14 23:50:34 +0700

    Update task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Read file
+
+let fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	console.log(data);
+});
